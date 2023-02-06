20f651ab41443b4b68419824199fc83ea24ad625
andrey <andreyfaigon@gmail.com>
2023-02-06 08:10:35 -0500

    Add project files.

---
+++ temperatureConverter/js/default.js
@@ -0,0 +1,11 @@
+﻿function fahrenheitToCelsius() {
+    let f = parseFloat(document.getElementById("fahrenheit").value);
+    let c = 5 / 9 * (f - 32);
+    document.getElementById("ftoc").innerHTML = c.toFixed(0) + "&deg" + "C";
+}
+function celsiusToFahrenheit() {
+    let c = parseFloat(document.getElementById("celsius").value);
+    let f = (9 / 5 * c) + 32;
+    document.getElementById("ctof").innerHTML = f.toFixed(0) + "&deg" + "F";
+
+}
